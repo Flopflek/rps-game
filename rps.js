d482677010f1858115c8ba54e9c90a87bd077704
Flopflek <sitko.b.szymon@gmail.com>
2023-12-12 15:30:14 +0000

    feat: add func to play round with values
    Added a function with a conditional if. The function will return a
    result based on the conditions. The draw option return has a replayRound
    function call but the function is yet to be written, so script will
    return an error. Also added a new variable for storing a user entered
    value which is used in the aforementioned function. A console.log line
    has been added to check the outputs of the function.

--- rps.js
+++ rps.js
@@ -4,12 +4,28 @@ function getComputerChoice() {                                     // function t
     return choice;
 }
 
+function playRound(playerSelection, computerSelection) {        // playRound runs a round with conditional outcomes. 
+if (playerSelection === computerSelection) {
+    return (replayRound())
+    
+}else if (playerSelection === "ROCK" && computerSelection === "SCISSORS") {
+    return ("You've won the round");
+
+}else if (playerSelection === "PAPER" && computerSelection === "ROCK") {
+    return ("You've won the round");
+
+}else if (playerSelection === "SCISSORS" && computerSelection === "PAPER") {
+    return ("You've won the round");
+
+}else 
+    return ("You've lost the round");
+
+}
 
     
         
-let computerSelection = getComputerChoice()
+let computerSelection = getComputerChoice()    //variable that calls on a function and stores the value.
+let playerSelection = prompt("Enter rock, paper, or scissors gamer").toUpperCase();  // variable that stores user entered value and converts to to uppercase.
 
-
-
-
+console.log(playRound(playerSelection, computerSelection)); //calls a function and logs the selected variables.
 
