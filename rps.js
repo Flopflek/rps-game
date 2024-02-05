4bdd2550aac0fe21b28cd8dbc21515eca8376ff2
Flopflek <sitko.b.szymon@gmail.com>
2024-02-05 20:45:20 +0000

    refactor! convert input and output into ui
    Converted all inputs and outputs into a ui using
    the DOM. refactored input and output functions to
    accommodate for the ui inputs. Added a play again
    feature.

--- rps.js
+++ rps.js
@@ -1,44 +1,86 @@
+
+const playButtonRock = document.createElement("button");
+playButtonRock.textContent = "ROCK";
+document.body.appendChild(playButtonRock);
+
+playButtonRock.addEventListener("click", choiceRock);
+
+function choiceRock() {
+    playerSelection = "ROCK";
+    computerSelection = getComputerChoice();
+    playRound(playerSelection, computerSelection);
+    
+}
+
+const playButtonPaper = document.createElement("button");
+playButtonPaper.textContent = "PAPER";
+document.body.appendChild(playButtonPaper);
+
+playButtonPaper.addEventListener("click", choicePaper);
+
+function choicePaper() {
+    playerSelection = "PAPER";
+    computerSelection = getComputerChoice();
+    playRound(playerSelection, computerSelection);
+}
+
+const playButtonScissors = document.createElement("button"); 
+playButtonScissors.textContent = "SCISSORS";
+document.body.appendChild(playButtonScissors);
+
+playButtonScissors.addEventListener("click", choiceScissors);
+
+function choiceScissors() {
+    playerSelection = "SCISSORS";
+    computerSelection = getComputerChoice();
+    playRound(playerSelection, computerSelection);
+}
+
+
+
 function getComputerChoice() {                                     // function that randomly selects one of the array values and returns it.
    const array = ["ROCK", "PAPER", "SCISSORS"];
    let choice = array[Math.floor(Math.random() * array.length)]; 
-    return choice;
+   return choice;
 }
 
-function game() {
-    
-    for (let i = 1; i <=5; i++) {
-        let computerSelection = getComputerChoice()    //variable that calls on a function and stores the value.
-        let playerSelection = prompt("Enter rock, paper, or scissors gamer").toUpperCase();       // variable that stores user entered value and converts to to uppercase.
-        let roundResult = playRound(playerSelection, computerSelection); //calls a function and stores the selected variable values.  
-        console.log(roundResult);
-        finalScore(roundResult);
-       
-    }      
-}
+
+
+//let computerSelection = getComputerChoice()    //variable that calls on a function and stores the value.
+  //      let playerSelection = prompt("Enter rock, paper, or scissors gamer").toUpperCase();       // variable that stores user entered value and converts to to uppercase.
+    //    let roundResult = playRound(playerSelection, computerSelection); //calls a function and stores the selected variable values.
 
 function playRound(playerSelection, computerSelection) {        // playRound runs a round with conditional outcomes. 
 if (playerSelection === computerSelection) {
-    return (replayRound())
+    div.textContent = "It's a tie! Play again.";
+    return
     
 }else if (playerSelection === "ROCK" && computerSelection === "SCISSORS") {
-    return (playerRoundWin);
+    div.textContent = (playerRoundWin);
+    finalScore(playerRoundWin);
+    return
 
 }else if (playerSelection === "PAPER" && computerSelection === "ROCK") {
-    return (playerRoundWin);
+    div.textContent = (playerRoundWin);
+    finalScore(playerRoundWin);
+    return
 
 }else if (playerSelection === "SCISSORS" && computerSelection === "PAPER") {
-    return (playerRoundWin);
+    div.textContent = (playerRoundWin);
+    finalScore(playerRoundWin);
+    return
 
 }else 
-    return (computerRoundWin);
-
+    div.textContent = (computerRoundWin);
+    finalScore(computerRoundWin);
+    return
 }
 
-function replayRound () {                                   // function is called on if a tie condition is met in the playround function, requests new values and calls on the playround function to play again.
-    computerSelection = getComputerChoice()
-    playerSelection = prompt("It's a tie! please enter rock, paper, or scissors again").toUpperCase();
-    return playRound(playerSelection, computerSelection);
-}
+//function replayRound () {                                   // function is called on if a tie condition is met in the playround function, requests new values and calls on the playround function to play again.
+  //  computerSelection = getComputerChoice()
+  //  playerSelection = addEventListener("click", choiceRock || choicePaper || choiceScissors);
+  //  return playRound(playerSelection, computerSelection);
+//}
 
 
 //final scoring function
@@ -46,19 +88,44 @@ function finalScore(roundResult) {
 
     if (roundResult === playerRoundWin) {
     (playerScore++);
-
-    }else (computerScore++);
+    playerDivScore.textContent = `Your score is ${playerScore}`;
     
+    }else (computerScore++);
+    compDivScore.textContent = `The computer's score is ${computerScore}`;
+
     if (playerScore === 3) {
-    console.log(playerWin)
+    finalResult.textContent = playerWin
 
     }else if (computerScore === 3) {
-    console.log(computerWin)
+    finalResult.textContent = computerWin
    
         }if (playerScore === 3 || computerScore === 3) {
-            return endGame()
+            const btnReset = document.createElement("button");
+            btnReset.textContent = "Play again";
+            document.body.appendChild(btnReset);
+            playButtonRock.removeEventListener("click", choiceRock);
+            playButtonPaper.removeEventListener("click", choicePaper);
+            playButtonScissors.removeEventListener("click", choiceScissors);
+            
+            btnReset.addEventListener("click", () => {
+                playerScore = 0;
+                computerScore = 0;
+                playerDivScore.textContent = `Your score is ${playerScore}`;
+                compDivScore.textContent = `The computer's score is ${computerScore}`;
+                finalResult.textContent = "";
+                document.body.removeChild(btnReset);
+                playButtonRock.addEventListener("click", choiceRock);
+                playButtonPaper.addEventListener("click", choicePaper);
+                playButtonScissors.addEventListener("click", choiceScissors);
+                
+            });
+          
         } 
+        
+        
     }
+
+
     
 //global variable declarations
 let computerScore = 0;
@@ -67,10 +134,23 @@ const playerWin = "Victory";
 const computerWin = "Defeat";
 const playerRoundWin = "You've won the round"; 
 const computerRoundWin = "You've lost the round";      
-        
 
 
+
+const div = document.createElement("div");
+div.textContent = "";
+document.body.appendChild(div);
+
+const playerDivScore = document.createElement("div");
+playerDivScore.textContent = `Your score is ${playerScore}`;
+document.body.appendChild(playerDivScore);
+
+const compDivScore = document.createElement("div");
+compDivScore.textContent = `The computer's score is ${computerScore}`;
+document.body.appendChild(compDivScore); 
+
+const finalResult = document.createElement("div");
+document.body.appendChild(finalResult); 
 //calls on the game function to execute
-game() 
-//ends the game 
-endGame()
+
+
